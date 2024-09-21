d65d673962439985b4a9127f65294b9a33824b61
namji95 <skawlgns07@naver.com>
2024-09-21 18:22:18 +0900

    1번 해답

---
+++ src/main/java/com/namji/codingtestpcce/PCCE_기출문제_1번_출력.java
@@ -0,0 +1,23 @@
+package com.namji.codingtestpcce;
+
+public class PCCE_기출문제_1번_출력 {
+  public static void main(String[] args) {
+    /*
+    주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다.
+    아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+    출력 예시
+    Spring is beginning
+    13
+    310
+     */
+
+    String msg = "Spring is beginning"; // 데이터 빈칸
+    int val1 = 3; // 데이터 빈칸
+    String val2 = val1 + ""; // 데이터 빈칸
+
+    System.out.println(msg);
+    System.out.println(val1 + 10);
+    System.out.println(val2 + "10");
+  }
+}
